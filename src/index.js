b93cf47ffd60a47c005c1d62e451abcbfb1819c3
Matteo Manchi <matteo.manchi@gmail.com>
2018-04-17 18:13:20 +0200

    First init

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text } from 'react-native';
+
+// const Amount = props => (
+//     <Text
+//         style={{ color: props.amount >= 0 ? 'green' : 'red' }}>
+//         € {props.amount.toFixed(2)}
+//     </Text>
+// );
+
+// export Amount;
+
+export const Amount = () => <Text>A</Text>
